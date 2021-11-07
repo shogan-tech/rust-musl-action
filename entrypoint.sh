4722c327b991ba60e378d46d808fad955fe798c4
Sean Duffy <shogan85@gmail.com>
2021-11-07 12:49:48 +0000

    add x86 macOs and windows targets

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,6 @@ cd $GITHUB_WORKSPACE
 rustup default stable
 rustup target add x86_64-unknown-linux-musl
 rustup target add aarch64-unknown-linux-musl
+rustup target add x86_64-apple-darwin
+rustup target add x86_64-pc-windows-gnu
 bash -c "$*"
