671e934ebdb920451108a1663d3dd3fc8bbb15bc
Sean <shogan85@gmail.com>
2021-11-07 13:25:45 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 set -Eeuxo pipefail
 cd $GITHUB_WORKSPACE
+
+apt install \
+    clang \
+    gcc \
+    g++ \
+    zlib1g-dev \
+    libmpc-dev \
+    libmpfr-dev \
+    libgmp-dev
+
+git clone https://github.com/tpoechtrager/osxcross
+cd osxcross
+wget -nc https://s3.dockerproject.org/darwin/v2/MacOSX10.10.sdk.tar.xz
+mv MacOSX10.10.sdk.tar.xz tarballs/
+UNATTENDED=yes OSX_VERSION_MIN=10.7 ./build.sh
+
+PATH="$(pwd)/osxcross/target/bin:$PATH"
+
 rustup default stable
 rustup target add x86_64-unknown-linux-musl
 rustup target add aarch64-unknown-linux-musl
